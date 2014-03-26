eb5fcfe5fbbd58771480c84d1c03550d9f409d97
Linh Pham <linhpham@Linhs-MacBook-Pro.local>
2014-03-26 11:32:39 -0700

    pushing

---
+++ search.js
@@ -0,0 +1,12 @@
+var button = true
+
+function toggle(filter){
+	var button = document.getElementById('more');
+	if button.style.display == true{
+		button.style.display = 'none';
+		button
+	else
+		button.style.display = 'inline-block';
+	}
+}, false);
+
